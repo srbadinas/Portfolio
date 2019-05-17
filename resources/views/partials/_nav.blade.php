2208ab5c94fb3d925487f962d9331a82e337ca3c
Sebastian Renz T. Badinas <srbadinas@gmail.com>
2019-05-17 08:20:44 +0800

    Navigation on small screen 40% completion

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -99,3 +99,65 @@
       </li>
   </ul>
 </nav>
+
+<nav class="navigation-small">
+  <div class="header">
+    <div class="toggle-button">
+      <a href="#">
+        <i class="glyphicon glyphicon-align-justify"></i>
+      </a>
+    </div>
+    <div class="header-content">
+        @if ($user_data)
+          <div class="person-content">
+              <div class="fullname">
+                  {{ $user_data->firstname }} {{ $user_data->lastname }}
+              </div>
+              <div class="title">
+                  {{ $user_data->career }}
+              </div>
+          </div>
+        @endif
+    </div>
+  </div>
+  <ul>
+    <li class="logo" onclick="location.href='{{ route('home') }}'">
+      
+    </li>
+    <li class="menu">
+      <a href="{{ route('about') }}">
+        <i class="fas fa-user teal-text"></i><span>About</span>
+      </a>
+    </li>
+    <li class="menu">
+        <a href="{{ route('project') }}">
+          <i class="fas fa-project-diagram red-text"></i><span>Projects</span>
+        </a>
+      </li>
+    <li class="menu">
+      <a href="{{ route('skills') }}">
+        <i class="fas fa-chart-bar purple-text"></i><span>Skills</span>
+      </a>
+    </li>
+    <li class="menu">
+      <a href="{{ route('experience') }}">
+        <i class="fas fa-chart-line cyan-text"></i><span>Experience</span>
+      </a>
+    </li>
+    <li class="menu">
+      <a href="{{ route('education') }}">
+        <i class="fas fa-graduation-cap orange-text"></i><span>Education</span>
+      </a>
+    </li>
+    <li class="menu">
+      <a href="#">
+        <i class="fas fa-envelope brown-text"></i><span>Contact</span>
+      </a>
+    </li>
+    <li class="menu">
+      <a href="#">
+        <i class="fas fa-file-alt gray-text"></i><span>Resume</span>
+      </a>
+    </li>
+  </ul>
+</nav>
