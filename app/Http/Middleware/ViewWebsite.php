03d4fd0742b71b66aad8e02fe61687386fadabc9
Sebastian Renz T. Badinas <srbadinas@gmail.com>
2019-06-04 15:23:52 +0800

    Added Viewer feature

--- app/Http/Middleware/ViewWebsite.php
+++ app/Http/Middleware/ViewWebsite.php
@@ -19,7 +19,26 @@ class ViewWebsite
     {
         $view = new Viewer();
         // $view->ip_address = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
-        $view->ip_address = file_get_contents('https://api.ipify.org');
+        
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN'; 
+
+        $view->ip_address = $ipaddress;
+
+
         $view->viewed_at = date('Y-m-d H:i:s');
         $view->save();
 
