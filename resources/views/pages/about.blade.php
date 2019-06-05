80eecf20ab38d3faa40b2acb8b2e920e96b2bd2e
Sebastian Renz T. Badinas <srbadinas@gmail.com>
2019-06-06 07:30:55 +0800

    Added FadeIn Animation

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,15 +14,15 @@
                 </div>
             @endif
         </div>
-        <div class="page-content text-reveal-animation">
+        <div class="page-content">
             <div class="about">
                 <div class="intro">
-                    <div class="content">
+                    <div class="content text-fade-in">
                         Hello. I'm Sebastian Badinas, a Web Developer from Philippines. Feel free to look around!
                     </div>
                 </div>
-                <div class="body">
-                    I have a deep passion in programming and I am craving for new knowledge about it. I always getting excited whenever I learned new things whether it's a tool/plugins that would make my work easily or a new programming language, coding structure and a framework which will be added in my skillset. I even watch tutorials and read documentations about programming in some of my free time. I am currently looking for a job in Singapore as a Web Developer.
+                <div class="body text-fade-in animation-delay-1">
+                    I have a deep passion in programming and I am craving for new knowledge about it. I always getting excited whenever I learned new things whether it's a tool/plugins that would make my work easily or a new programming language, coding structure and a framework which will be added to my skillset. I even watch tutorials and read documentations about programming in some of my free time. I am currently looking for a job overseas as a Web Developer.
                 </div>
             </div>
         </div>
